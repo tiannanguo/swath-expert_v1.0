1c20f7ad1adc7677ec5cf5a2e536986192a452f8
Tiannan Guo <tiannan.guo@gmail.com>
2015-11-11 21:30:14 +0100

    add parameters.py

---
+++ parameters.py
@@ -0,0 +1,11 @@
+__author__ = 'Tiannan Guo, ETH Zurich 2015'
+
+# parameters
+PEAK_TOLERANCE = 6  # seconds, use to find if the peak is found
+title = ['transition_name','transition_group_id', 'best_rt', 'best_sample', 'best_score', 'protein', 'irt', 'precursor_mz', 'transition_mz', 'transition_i']
+
+MIN_FRAGMENTS = 4
+MAX_RT_TOLERANCE = 10
+MAX_PEAK_WIDTH = 100
+PEAK_WIDTH_FOLD_VARIATION = 2.0
+PEAK_SHAPE_FOLD_VARIATION = 3.0
