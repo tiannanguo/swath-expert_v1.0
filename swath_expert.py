57b883440e07b8b4e74f1272fda6970619f117b9
Tiannan Guo <tiannan.guo@gmail.com>
2015-12-16 13:40:14 +0100

    v0.164. release and run the batch of nci60

--- swath_expert.py
+++ swath_expert.py
@@ -14,16 +14,16 @@ import parameters
 # name of files
 # chrom_file = 'com_chrom_10_test.txt.gz'    #sys.argv[1]
 # chrom_file = 'com_chrom_8.txt.gz'    #sys.argv[1]
-# chrom_file = sys.argv[1]
-# platform = 'linux'
-platform = 'windows'
-chrom_file = 'debug_nci60_png_id_29310.txt.gz'
+chrom_file = sys.argv[1]
+platform = 'linux'
+# platform = 'windows'
+# chrom_file = 'com_chrom_3_46.txt.gz'
 # chrom_file = 'debug_png_id_620.txt.gz'
 # chrom_file = 'com_chrom_5.txt.gz'
 # id_mapping_file = 'goldenSets90.txt'
 id_mapping_file = 'nci60sw.txt'
 tic_normalization_file = 'nci60.tic'
-sample_replicates_info_file = 'nci60_replicates_info.txt'
+# sample_replicates_info_file = 'nci60_replicates_info.txt'
 # id_mapping_file = 'goldenSets90_test.txt'
 out_R_file = chrom_file.replace('.txt.gz', '.R')
 out_file_poor_tg = chrom_file.replace('.txt.gz', '.poor.txt')
