cd374a5b31ac1dabc88f673224cc87958450e79a
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-12-23 15:24:42 +0100

    refactored _get_all_tg_in_chrom_gold.py

--- _get_all_tg_in_chrom_gold.py
+++ _get_all_tg_in_chrom_gold.py
@@ -1,18 +1,15 @@
 import csv
 import gzip
 
-tg = {}
-for i in range(1,33):
-    with gzip.open('com_chrom_' + str(i) + '.txt.gz', 'rb') as i:
-        r = csv.DictReader(i, delimiter="\t")
-
+tg = set()
+for i in range(1, 33):
+    with gzip.open('com_chrom_%d.txt.gz' % i, 'rb') as fh:
+        r = csv.DictReader(fh, delimiter="\t")
         for row in r:
+            tg.add(row['transition_group_id'])
 
-            tg[row['transition_group_id']] = 1
 
-
-with open('_all_tg_gold90.txt', 'wb') as OUT:
-
-    for tg_id in tg.keys():
+with open('_all_tg_gold90.txt', 'wb') as fh:
+    for tg_id in tg:
         print tg_id
-        OUT.write(tg_id + "\n")
+        fh.write(tg_id + "\n")
